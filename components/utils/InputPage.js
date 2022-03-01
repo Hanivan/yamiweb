b695969848bec86854f3bad8932ab952d023b40e
Hanivan <hanivan20@gmail.com>
2022-03-01 16:44:53 +0700

    feat: better ux input page

--- components/utils/InputPage.js
+++ components/utils/InputPage.js
@@ -9,7 +9,7 @@ export default function InputPage({ currentPage, setter }) {
     <input
       className="border text-sm border-murasakino p-1.5 w-16 rounded bg-yami-900 text-center transition-all focus:outline-none focus:ring-1 focus:ring-murasakino"
       id="inputPage"
-      placeholder={`${currentPage ? currentPage : "- of -"}`}
+      placeholder={`${currentPage ? currentPage : "1 of 1"}`}
       onChange={(e) => getPage(e)}
     />
   );
